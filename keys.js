265d1af6c30e315711da9afafca054eee5cbb9b9
Roman Voropaev <voropaev.roma@gmail.com>
2020-03-27 00:14:12 +0300

    Formatting

--- keys.js
+++ keys.js
@@ -1,12 +1,12 @@
-const prefix = 'paranoid'; 
+const prefix = 'paranoid';
 function check_key(user_id, key) {
-  if(!key) console.error("empty key for", user_id);
+  if (!key) console.error("empty key for", user_id);
 }
 
 function get_public(user_id) {
   const key = `${prefix}:public:${user_id}`;
   const res = localStorage.getItem(key);
-  check_key(user_id,res);
+  check_key(user_id, res);
   console.debug(user_id, res);
   return res;
 }
@@ -15,10 +15,11 @@ function get_private(user_id, password) {
   const key = `${prefix}:private:${user_id}`;
   const res = localStorage.getItem(key);
   // TODO: key decipher
-  check_key(user_id,res);
+  check_key(user_id, res);
   return res;
 }
 
 module.exports = {
-  get_public, get_private
+  get_public,
+  get_private
 }
--- main.js
+++ main.js
@@ -8,12 +8,8 @@ const myself = new jsencrypt.JSEncrypt();
 const companion = new jsencrypt.JSEncrypt();
 
 // Determine interlocutors using picture links in UI
-const my_id = 
-  document.querySelector('a.top_profile_link').
-  attributes['href'].value.substr(1);
-const his_id = 
-  document.querySelector('.im-page--aside-photo a').
-  attributes.href.value.substr(1);
+const my_id = document.querySelector('a.top_profile_link').attributes['href'].value.substr(1);
+const his_id = document.querySelector('.im-page--aside-photo a').attributes.href.value.substr(1);
 console.debug(my_id, his_id);
 
 // Load keys upfront
@@ -22,24 +18,24 @@ myself.setPrivateKey(keys.get_private(my_id, 'passwd'));
 companion.setPublicKey(keys.get_public(his_id));
 
 // First - encrypt input messagebox on mouse leave
-var old_input_content="";
+var old_input_content = "";
 var inputbox = document.querySelector(".im-chat-input--text");
 
 inputbox.onmouseout = function(e) {
-    if (e.target.innerText) {
-        console.debug(e.target.innerText);
-        old_input_content = e.target.innerText;
-        raw_text = old_input_content;
-        console.log(raw_text.length)
-        encrypted = companion.encrypt(raw_text)
-        encrypted = encrypted + ' ' + myself.sign(crypto.MD5(encrypted),
-            crypto.MD5, "md5");
-        e.target.innerText=you.encrypt(encrypted);
-    }
+  if (e.target.innerText) {
+    console.debug(e.target.innerText);
+    old_input_content = e.target.innerText;
+    raw_text = old_input_content;
+    console.log(raw_text.length)
+    encrypted = companion.encrypt(raw_text)
+    encrypted = encrypted + ' ' + myself.sign(crypto.MD5(encrypted),
+      crypto.MD5, "md5");
+    e.target.innerText = you.encrypt(encrypted);
+  }
 }
 inputbox.onmouseover = inputbox.onchange = function(e) {
-    if (old_input_content) 
-        e.target.innerText=old_input_content;
+  if (old_input_content)
+    e.target.innerText = old_input_content;
 }
 
 // Second - decrypt messages on mouse over
@@ -47,28 +43,28 @@ var message_hist = document.querySelector(".im-page-chat-contain");
 var old_message_content = new Map();
 
 message_hist.onmouseover = function(e) {
-    const elem = e.target;
-    if (elem.matches(".im-mess--text")) { 
-        const msgid = elem.parentNode.dataset["msgid"];
-        old_message_content.set(msgid, elem.innerText);
-        decrypted = myself.decrypt(elem.innerText);
-        if (decrypted) {
-            var words = decrypted.split(' ');
-            var signature = atob(words[words.length-1]);
-            var raw_text = decrypted.slice(0, decrypted);
-            if (companion.verify(raw_text, signature, crypto.MD5)) {
-                console.log("signature verified");
-                elem.innerText = raw_text;
-            } else {
-                console.log("signature failed");
-            }
-        }
+  const elem = e.target;
+  if (elem.matches(".im-mess--text")) {
+    const msgid = elem.parentNode.dataset["msgid"];
+    old_message_content.set(msgid, elem.innerText);
+    decrypted = myself.decrypt(elem.innerText);
+    if (decrypted) {
+      var words = decrypted.split(' ');
+      var signature = atob(words[words.length - 1]);
+      var raw_text = decrypted.slice(0, decrypted);
+      if (companion.verify(raw_text, signature, crypto.MD5)) {
+        console.log("signature verified");
+        elem.innerText = raw_text;
+      } else {
+        console.log("signature failed");
+      }
     }
+  }
 }
 message_hist.onmouseout = function(e) {
-    const elem = e.target;
-    if (elem.matches(".im-mess--text")) { 
-        const msgid = elem.parentNode.dataset["msgid"];
-        elem.innerText=old_message_content.get(msgid);
-    }
+  const elem = e.target;
+  if (elem.matches(".im-mess--text")) {
+    const msgid = elem.parentNode.dataset["msgid"];
+    elem.innerText = old_message_content.get(msgid);
+  }
 }
