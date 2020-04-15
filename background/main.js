c1444114d58acabce1b5ca106d5e0f798510d9cd
Roman Voropaev <voropaev.roma@gmail.com>
2020-04-15 17:10:41 +0300

    Update background script for chrome

--- background/main.js
+++ background/main.js
@@ -1,4 +1,7 @@
+const browser = chrome || browser;
+
 browser.browserAction.onClicked.addListener(function() {
+  console.info("paranoid extension reloading");
   browser.runtime.reload();
   browser.tabs.reload();
 });
