be2d8b35b53f6cc1c73790328ced9ebf98987079
Roman Voropaev <voropaev.roma@gmail.com>
2020-05-08 23:30:17 +0300

    Selenium test with 2 browsers

--- func_test/main.py
+++ func_test/main.py
@@ -8,12 +8,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.remote.command import Command
 
-if env["BROWSER"] == "firefox":
-    bro1 = webdriver.Firefox(firefox_binary=env["FIREFOX_BINARY"])
-    bro2 = webdriver.Firefox(firefox_binary=env["FIREFOX_BINARY"])
-else:
-    bro1 = webdriver.Chrome()
-    bro2 = webdriver.Chrome()
+bro1 = webdriver.Firefox(firefox_binary=env["FIREFOX_BINARY"])
+bro2 = webdriver.Firefox(firefox_binary=env["FIREFOX_BINARY"])
 
 bro1.install_addon(os.path.abspath(
     f"../dist/paranoid-{env['ADDON_VERSION']}-an+fx.xpi"))
@@ -21,7 +17,7 @@ bro2.install_addon(os.path.abspath(
     f"../dist/paranoid-{env['ADDON_VERSION']}-an+fx.xpi"))
 
 bro1.set_window_position(0, 0)
-bro2.set_window_position(700, 200)
+bro2.set_window_position(500, 100)
 
 def login_vk(bro, login, password):
     bro.get("https://vk.com")
@@ -52,32 +48,40 @@ def send_message(bro):
 
 def import_key(bro, user_id, val):
     key = f"paranoid:public:{user_id}"
-    bro.execute(Command.SET_LOCAL_STORAGE_ITEM, {"key": key, "val": val})
+    script = f"localStorage.setItem('{key}', `{val}`)"
+    bro.execute_script(script)
 
 def export_key(bro, user_id):
     key = f"paranoid:public:{user_id}"
-    return bro.execute(Command.GET_LOCAL_STORAGE_ITEM, {"key": key})
+    script = f"return localStorage.getItem('{key}')"
+    return bro.execute_script(script)
 
 try:
-    user1_id = env["USER1_ID"]
-    user2_id = env["USER2_ID"]
+    user1_name = env["USER1_NAME"]
+    user2_name = env["USER2_NAME"]
     login_vk(bro1, env["USER1_LOGIN"], env["USER1_PASSWORD"])
     login_vk(bro2, env["USER2_LOGIN"], env["USER2_PASSWORD"])
     sleep(3)
-    bro1.get(f"https://vk.com/im?sel={user2_id}")
-    bro2.get(f"https://vk.com/im?sel={user1_id}")
+    bro1.get(f"https://vk.com/im?sel={env['USER2_ID']}")
+    bro2.get(f"https://vk.com/im?sel={env['USER1_ID']}")
     sleep(1)
 
     bro1_key = accept_key(bro1)
     bro2_key = accept_key(bro2)
-    import_key(bro1, user2_id, bro2_key)
-    import_key(bro2, user1_id, bro1_key)
+    import_key(bro1, user2_name, bro2_key)
+    import_key(bro2, user1_name, bro1_key)
 
-    print(export_key(bro1, user2_id))
-    print(export_key(bro2, user1_id))
+    print(user2_name, export_key(bro1, user2_name))
+    print(user1_name, export_key(bro2, user1_name))
+
+    bro1.refresh()
+    bro2.refresh()
+    sleep(3)
+    send_message(bro1)
+    send_message(bro2)
 
 finally:
-    # sleep(20)
+    sleep(10)
     bro1.quit()
     bro2.quit()
 
